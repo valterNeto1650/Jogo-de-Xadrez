9db3d9f6ad2ea1bf61dbbf1ddd63be73f5a0e327
valterNeto1650 <valterneto@id.uff.br>
2023-06-01 15:35:03 -0300

    Implementação do menu

--- main/menu.py
+++ main/menu.py
@@ -1,87 +1,52 @@
 import pygame
-from pygame.locals import *
-from sys import exit
+import sys
 
+# Cores
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+
+# Inicialização do Pygame
 pygame.init()
 
-# Atributos da tela
-
+# Dimensões da tela
 largura = 800
-altura = 600
+altura = 500
 
-larguraBotao = 250
-alturaBotao = 50
-
-larguraPeca = 50
-alturaPeca = 50
-
-posicaoXBotao = 280
-posicaoYBotao = 420
-
-tamanho = largura , altura
-tamanhoBotao = larguraBotao, alturaBotao
-#tamanhoPeca = larguraPeca, alturaPeca
-
+# Criar a tela
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo de Xadrez")
 
-fundo = pygame.image.load("assets/fundo/telaMenu.png").convert()
-fundo = pygame.transform.scale(fundo, tamanho)
+# Carregar imagem
+imagem = pygame.image.load("assets/fundo/telaMenu.png")  # Substitua "imagem.png" pelo caminho para sua imagem
 
-fundoJogo = pygame.image.load("assets/fundo/telaGame2.png").convert()
-fundoJogo = pygame.transform.scale(fundoJogo, tamanho)
+# Botão "Jogar"
+botao_jogar = pygame.Rect(largura // 2 - 50, altura - 100, 100, 50)
 
-botaoJogar = pygame.image.load("assets/fundo/botaoJogar.png").convert()
-botaoJogar = pygame.transform.scale(botaoJogar, tamanhoBotao)
-retangulo = botaoJogar.get_rect()
-
-"""
-bispoPreto = pygame.image.load("assets/peças/black_bishop.png").convert()
-bispoPreto = pygame.transform.scale(bispoPreto, tamanhoPeca)
-
-reiPreto = pygame.image.load("assets/peças/black_king.png").convert()
-reiPreto = pygame.transform.scale(reiPreto, tamanhoPeca)
-
-cavaloPreto = pygame.image.load("assets/peças/black_knight.png").convert()
-cavaloPreto = pygame.transform.scale(cavaloPreto, tamanhoPeca)
-
-peaoPreto = pygame.image.load("assets/peças/black_pawn.png").convert()
-peaoPreto = pygame.transform.scale(peaoPreto, tamanhoPeca)
-
-rainhaPreto = pygame.image.load("assets/peças/black_queen.png").convert()
-rainhaPreto = pygame.transform.scale(rainhaPreto, tamanhoPeca)
-
-torrePreto = pygame.image.load("assets/peças/black_rook.png").convert()
-torrePreto = pygame.transform.scale(torrePreto, tamanhoPeca)
-"""
-
-retangulo.x = posicaoXBotao
-retangulo.y = posicaoYBotao
-
-click = False
-
+# Loop principal do jogo
 while True:
-   
-    
-    for  event in pygame.event.get():
-        if event.type == QUIT:
+    # Tratar eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if botao_jogar.collidepoint(event.pos):
+                # Redirecionar para o jogo.py
+                exec(open("jogo.py").read()) 
 
-        # Verifique se o botão esquerdo do mouse foi clicado dentro do retângulo
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if retangulo.collidepoint(event.pos):
-                click = True 
-                print("Imagem clicada!")
-        
+    # Preencher a tela com a cor branca
+    tela.fill(BRANCO)
 
-    if click:
-            tela.blit(fundoJogo, (0, 0))
-            
-    else:
-        tela.blit(fundo, (0, 0))
-        tela.blit(botaoJogar, (posicaoXBotao, posicaoYBotao))
-    #Desenha a tela
-    
-    
-    pygame.display.update()
+    # Desenhar a imagem 
+   
+    tela.blit(imagem, (0, 0))
+
+    # Desenhar o botão "Jogar"
+    pygame.draw.rect(tela, PRETO, botao_jogar)
+    fonte = pygame.font.SysFont(None, 30)
+    texto = fonte.render("Jogar", True, BRANCO)
+    texto_rect = texto.get_rect(center=botao_jogar.center)
+    tela.blit(texto, texto_rect)
+
+    # Atualizar a tela
+    pygame.display.flip()
